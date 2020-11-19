e1144d41f6da6b2a049e65c81abd52a4e15969f9
haple <aleph.oliveira@etec.sp.gov.br>
2020-11-19 20:26:52 -0300

    Ajustado controller para evitar consultas de receitas com mais de 3 ingredientes

--- src/modules/recipes/infra/http/controllers/RecipesController.ts
+++ src/modules/recipes/infra/http/controllers/RecipesController.ts
@@ -2,10 +2,15 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import SearchRecipesService from '@modules/recipes/services/SearchRecipesService';
+import AppError from '@shared/errors/AppError';
 
 export default class RecipesController {
   public async index(request: Request, response: Response): Promise<Response> {
     const ingredients = String(request.query.i).split(',');
+
+    if (ingredients.length > 3) {
+      throw new AppError('Please, provide 3 ingredients or less');
+    }
 
     const searchRecipes = container.resolve(SearchRecipesService);
 
